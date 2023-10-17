b51c0709ffb33c873b8b7c72807cfc5db947e9ec
Nikhil Gupta <nikhil.gupta5667@gmai.com>
2023-10-17 19:41:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="tracking-tighter">
+      <h1 className="text-3xl font-bold">Watch Wiz!</h1>
+      <p className="text-xl text-gray-900">Discover. Stream. Enjoy.</p>
+    </div>
+  )
+}
+
+export default App
